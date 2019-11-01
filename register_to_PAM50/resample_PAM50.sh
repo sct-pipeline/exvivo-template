bb76a50a5962d75e6ad5e37b6056a0c03316fc22
charley.gros <charley.gros@gmail.com>
2019-11-01 00:19:26 -0400

    register_to_PAM50/resample_PAM50: crop before resample

--- register_to_PAM50/resample_PAM50.sh
+++ register_to_PAM50/resample_PAM50.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # This script resample the PAM50 files to an input resolution.
+# Before resampling, a cropping around the region of interest is done
+#	in order to speed up the resampling.
 #
 # Usage:
 #   ./resample_PAM50.sh <resolution_mm> <ofolder>
@@ -19,12 +21,19 @@ mkdir -p $ofolder
 # PAM50 folder path
 pam50path="$(echo $SCT_DIR/data/PAM50/template/)"
 
+# create mask
+sct_create_mask -i "$pam50path"/PAM50_centerline.nii.gz -p center -size 40 -f box -o "$ofolder"/mask.nii.gz
+
 # loop across PAM50 files
 for file in "$pam50path"*.nii.gz; do
     echo "$file"
     ofile="$ofolder"/"$(basename "$file")"
     if [ ! -f $ofile ]; then
+        # Crop around the spinal cord
+        sct_crop_image -i $file -o $ofile -m "$ofolder"/mask.nii.gz
+        # Crop from T3/T2
+        sct_crop_image -i $ofile -o $ofile -zmin 648
         # Resample
-        sct_resample -i $file -o $ofile -mm "$resolution"x"$resolution"x"$resolution"
+        sct_resample -i $ofile -o $ofile -mm "$resolution"x"$resolution"x"$resolution"
     fi
 done
